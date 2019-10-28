83c6ccf6805151d07dca1d48ed79a16c7b60abb6
Gabriel Araujo <gabriiel66@gmail.com>
2019-10-28 20:49:42 -0300

    ajuste fs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
+const fs = require('fs')
 const db = require('./config/db')
 require('dotenv').config()
 
